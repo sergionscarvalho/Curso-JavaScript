1c4795637033031493105d873284fa5680f39885
Sergio Carvalho <SC@PORTUGAL>
2018-08-06 22:42:51 +0100

    exemplo de Var

---
+++ fundamentos/usandoVar1.js
@@ -0,0 +1,20 @@
+{
+    {
+        {
+            {
+                 var sera = 'Será???'; 
+                 console.log(sera);
+            }
+        }
+    }
+}
+
+console.log(sera);
+
+function teste() {
+    var local = 123;
+    console.log(local);
+}
+
+teste();
+// console.log(local); quando o var é criada dentro de uma function só é visivel nessa function
